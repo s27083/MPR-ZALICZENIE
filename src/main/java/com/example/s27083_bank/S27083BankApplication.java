081f6009702711ad17e6cae9474f8066d9800cdb
Kamil Bogdański <kamilbogdanski@MacBook-Pro-Kamil-2.local>
2024-01-28 15:08:49 +0100

    init commit

---
+++ src/main/java/com/example/s27083_bank/S27083BankApplication.java
@@ -0,0 +1,13 @@
+package com.example.s27083_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S27083BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S27083BankApplication.class, args);
+	}
+
+}
